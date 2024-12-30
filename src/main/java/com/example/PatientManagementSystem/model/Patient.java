5fcac6f531c00e939c67a3d8ee800f86f032279c
Hrishikesh1723 <hrishikesh.r@ahduni.edu.in>
2024-12-30 13:56:25 +0530

    Patient model updated

--- src/main/java/com/example/PatientManagementSystem/model/Patient.java
+++ src/main/java/com/example/PatientManagementSystem/model/Patient.java
@@ -3,8 +3,13 @@ package com.example.PatientManagementSystem.model;
 import java.util.List;
 import java.time.LocalDate;
 import jakarta.validation.constraints.*;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="patients")
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "Name is mandatory")
@@ -16,7 +21,12 @@ public class Patient {
     @Pattern(regexp = "\\d{10}", message = "Phone number should be 10 digits")
     private String phone;
 
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<String> reportList;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
 
     @PastOrPresent(message = "Admit date must be in the past or present")
     private LocalDate admitDate;
@@ -32,6 +42,8 @@ public class Patient {
     public void setPhone(String phone) { this.phone = phone; }
     public List<String> getReportList() { return reportList; }
     public void setReportList(List<String> reportList) { this.reportList = reportList; }
+    public Doctor getDoctor() {return  doctor; }
+    public void setDoctor(Doctor doctor) { this.doctor = doctor; }
     public LocalDate getAdmitDate() { return admitDate; }
     public void setAdmitDate(LocalDate admitDate) { this.admitDate = admitDate; }
 }
---
+++ src/main/java/com/example/PatientManagementSystem/model/Report.java
@@ -0,0 +1,4 @@
+package com.example.PatientManagementSystem.model;
+
+public class Report {
+}
