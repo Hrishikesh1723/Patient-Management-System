9fa0fe1a5543e302e95918d68bb9cba6aceeb623
Hrishikesh1723 <hrishikesh.r@ahduni.edu.in>
2024-12-30 19:48:42 +0530

    Appointment DAO added

--- src/main/java/com/example/PatientManagementSystem/dao/AppointmentDAO.java
+++ src/main/java/com/example/PatientManagementSystem/dao/AppointmentDAO.java
@@ -1,4 +1,7 @@
 package com.example.PatientManagementSystem.dao;
 
-public class AppointmentDAO {
+import com.example.PatientManagementSystem.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentDAO extends JpaRepository<Patient, Long>{
 }
